72a944d534249009afb98c24361467d0e6e67c3b
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-03-22 19:11:05 -0700

    Add interactive map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,48 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+
 const Map = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
+    const { cities } = useCities();
+
+    const [mapPosition, setMapPosition] = useState([40, 0]);
 
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
 
     return (
-        <div className={styles.mapContainer} onClick={() => {navigate("form")}}>
-            <h2>Map</h2>
-            <h3>{lat}, {lng}</h3>
+        <div className={styles.mapContainer}>
+            <MapContainer
+                center={mapPosition}
+                zoom={13}
+                scrollWheelZoom={true}
+                className={styles.map}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                />
+                {cities.map((city) => {
+                    const coordinates = [city.position.lat, city.position.lng];
+
+                    return (
+                        <Marker
+                            position={coordinates}
+                            key={city.id}
+                        >
+                            <Popup>
+                                <p>{city.cityName} {city.country}</p>
+                            </Popup>
+                        </Marker>
+                    );
+                })}
+            </MapContainer>
         </div>
     );
 };
